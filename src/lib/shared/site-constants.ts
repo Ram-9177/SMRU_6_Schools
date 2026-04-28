77526dddd4340d4e918b78e2a9b94a68b350aedd
Polakam Sri Ram <polakamsriram@stmarysgroup.com>
2026-04-28 22:12:47 +0530

    Update official social links and clean PhD apply URL

--- src/lib/shared/site-constants.ts
+++ src/lib/shared/site-constants.ts
@@ -7,8 +7,7 @@ export const SITE_CONTACT = {
     "Near Ramoji Film City, Deshmukhi Village, Pochampally Mandal, Yadadri Bhuvanagiri District, Hyderabad, Telangana - 508284, India.",
 } as const;
 
-export const PHD_APPLY_URL =
-  "https://apply.smru.edu.in/Smru/phd/?&utm_source=GoogleAds&utm_network=g&utm_medium=c&utm_keyword=phd%20university&utm_location=9302592&utm_campaign_id=23568608827&utm_source=GoogleAds&utm_network=g&utm_medium=c&utm_keyword=phd%20university&utm_location=9302592&utm_campaign_id=23568608827&utm_adset_id=191050073497&utm_ad_id=797345978535&matchtype=p&placement=&gclid=Cj0KCQjw4a3OBhCHARIsAChaqJMkJqNXMSCPwtJmNnmNQVC-5wrZqINw9I19eZ8QFDhVYIANGphS8y4aAiXuEALw_wcB&gad_source=1&gad_campaignid=23568608827&gbraid=0AAAABCWg9mqefnSTPsaw089RiOQXjxeng";
+export const PHD_APPLY_URL = "https://apply.smru.edu.in/Smru/phd/";
 
 export const SITE_CTA_LINKS = {
   apply: "https://apply.smru.edu.in",
@@ -31,10 +30,10 @@ export const resolveApplyLink = ({
 };
 
 export const SITE_SOCIAL_LINKS = {
-  facebook: "https://www.facebook.com/",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  youtube: "https://www.youtube.com/",
+  facebook: "https://www.facebook.com/SMRUniversity",
+  instagram: "https://www.instagram.com/smruhyderabad",
+  linkedin: "https://www.linkedin.com/company/smruhyderabad/",
+  youtube: "https://www.youtube.com/@SMRUniversity",
 } as const;
 
 export const PARTNER_HIDDEN_STICKY_ROUTES = [] as const;
